41724659750704a7f478873180ec23c961a2ae32
soliavkun <alexander.sollyavkun.acceltime@gmail.com>
2019-12-24 19:53:44 +0200

    Update Arcanoid.py

--- Arcanoid.py
+++ Arcanoid.py
@@ -1,64 +1,105 @@
-import sys, pygame, time
-pygame.init()
+import sys, pygame
+from random import randrange
 
 FPS = 30
 SIZE = WIDTH, HEIGHT = 640, 480
 speed = [1, 1]
 BLACK = 0, 0, 0
 
-screen = pygame.display.set_mode(SIZE)
-
-class Ball:
+class Ball(pygame.sprite.Sprite):
     def __init__(self):
-        self.img = pygame.image.load("intro_ball.gif")
-        self.rect = self.img.get_rect()
+        super().__init__()
+        self.image = pygame.image.load("intro_ball.gif")
+        self.mask = pygame.mask.from_surface(
+            pygame.image.load("intro_ball_mask.gif"))
+        self.rect = self.image.get_rect()
+        self.name = 'Ball'
         self.n = 0
 
     def change(self):
         self.n += 1
         xy = self.rect.center
         if self.n == 3:
-            self.img = pygame.image.load("intro_ball.gif")
-            self.rect = self.img.get_rect()
+            self.image = pygame.image.load("intro_ball.gif")
+            self.mask = pygame.mask.from_surface(
+                pygame.image.load("intro_ball_mask.gif"))
+            self.mask = pygame.mask.from_surface(self.image)
+            self.rect = self.image.get_rect()
             self.rect.center = xy
             self.n = 0
         if self.n == 1:
-            self.img = pygame.image.load("intro_ball_middle.gif")
-            self.rect = self.img.get_rect()
+            self.image = pygame.image.load("intro_ball_middle.gif")
+            self.mask = pygame.mask.from_surface(self.image)
+            self.rect = self.image.get_rect()
             self.rect.center = xy
         if self.n == 2:
-            self.img = pygame.image.load("intro_ball_small.gif")
-            self.rect = self.img.get_rect()
+            self.image = pygame.image.load("intro_ball_small.gif")
+            self.mask = pygame.mask.from_surface(self.image)
+            self.rect = self.image.get_rect()
             self.rect.center = xy
 
-ball = Ball()
 
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.display.quit()
-            pygame.quit()
-            sys.exit('See you next time!')
+class App():
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                speed[0] += 1
-            if event.key == pygame.K_LEFT:
-                speed[0] -= 1
-            if event.key == pygame.K_UP:
-                speed[1] -= 1
-            if event.key == pygame.K_DOWN:
-                speed[1] += 1
-            if event.key == pygame.K_SPACE:
-                ball.change()
+    def __init__(self):
+        pygame.init()
+        screen = pygame.display.set_mode(SIZE)
 
-    ball.rect = ball.rect.move(speed)
-    if ball.rect.left < 0 or ball.rect.right > WIDTH:
-        speed[0] = -speed[0]
-    if ball.rect.top < 0 or ball.rect.bottom > HEIGHT:
-        speed[1] = -speed[1]
+    def make_ball(self):
+        main_ball = Ball()
 
-    screen.fill(BLACK)
-    screen.blit(ball.img, ball.rect)
-    pygame.display.flip()
-    pygame.time.Clock().tick(FPS)
+    def terminate(self):
+        pygame.display.quit()
+        pygame.quit()
+        sys.exit('See you next time!')
+
+    def main_loop(self):
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.terminate()
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        main_ball.change()
+
+                main_ball.rect.center = pygame.mouse.get_pos()
+                if main_ball.rect.left < 0:
+                    main_ball.rect.left = 0
+                elif main_ball.rect.right > WIDTH:
+                    main_ball.rect.right = WIDTH
+                if main_ball.rect.top < 0:
+                    main_ball.rect.top = 0
+                elif main_ball.rect.bottom > HEIGHT:
+                    main_ball.rect.bottom = HEIGHT
+                
+                for ball in balls_list:
+                    if pygame.sprite.collide_mask(main_ball, ball):
+                        print(ball.name)
+                        ball.kill()
+                #pygame.sprite.spritecollide(main_ball, balls_list, True)
+
+            screen.fill(BLACK)
+            all_sprites.draw(screen)
+            pygame.display.flip()
+            pygame.time.Clock().tick(FPS)
+
+balls_list = pygame.sprite.Group()
+
+for i in range(1, 17):
+    ball = Ball()
+    ball.change()
+    ball.change()
+    ball.name += '_' + str(i)
+    ball.rect.x = randrange(WIDTH - ball.rect.width)
+    ball.rect.y = randrange(HEIGHT - ball.rect.height)
+    
+    balls_list.add(ball)
+
+all_sprites = pygame.sprite.Group(balls_list, main_ball)
+
+
+if __name__ == '__main__':
+    app = App()
+    app.main_loop()
+    
